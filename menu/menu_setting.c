19f28167f7bc9ac347fff4b0c043befbd221981e
JoseFC3000 <70484443+JoseFC3000@users.noreply.github.com>
2024-11-23 04:07:29 +0000

    Update menu_setting.c

--- menu/menu_setting.c
+++ menu/menu_setting.c
@@ -9809,7 +9809,7 @@ static bool setting_append_list(
                general_write_handler,
                general_read_handler);
          (*list)[list_info->index - 1].action_ok     = &setting_action_ok_uint;
-         (*list)[list_info->index - 1].offset_by     = -1;
+         (*list)[list_info->index - 1].offset_by     = 0;
          (*list)[list_info->index - 1].get_string_representation =
             &setting_get_string_representation_state_slot;
          menu_settings_list_current_add_range(list, list_info, 0, 20, 1, true, true);
