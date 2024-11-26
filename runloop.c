e3e382568fca03f4577664d9c546cfce1c40f332
JoseFC3000 <70484443+JoseFC3000@users.noreply.github.com>
2024-11-26 17:34:54 +0000

    Update runloop.c

--- runloop.c
+++ runloop.c
@@ -6592,7 +6592,7 @@ static enum runloop_state_enum runloop_check_state(
       if (!check2)
       {
          check2                            = should_slot_decrease && !old_should_slot_decrease;
-         check1                            = state_slot > 0;
+         check1                            = state_slot > 1;
          addition                          = -1;
 
          /* Wrap-around to 999 */
