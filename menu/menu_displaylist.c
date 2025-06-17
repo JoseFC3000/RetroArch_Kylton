64d10ba6847505fef1be52fafdf6981d7d811d0b
JoseFC3000 <70484443+JoseFC3000@users.noreply.github.com>
2025-06-17 20:24:50 +0100

    Update menu_displaylist.c

--- menu/menu_displaylist.c
+++ menu/menu_displaylist.c
@@ -5259,6 +5259,27 @@ static unsigned menu_displaylist_parse_disk_options(file_list_t *list)
                MENU_ENUM_LABEL_DISK_TRAY_EJECT,
                MENU_SETTINGS_CORE_DISK_OPTIONS_DISK_CYCLE_TRAY_STATUS, 0, 0, NULL))
          count++;
+
+   /* If core does not support appending images,
+    * can stop here */
+   if (!disk_control_append_enabled(&sys_info->disk_control))
+      return count;
+
+   /* Always show a 'DISK_IMAGE_APPEND' entry
+    * > If tray is currently shut, this will:
+    *   - Open tray
+    *   - Append disk image
+    *   - Close tray
+    * > If tray is currently open, this will
+    *   only append a disk image */
+   if (menu_entries_append(list,
+            msg_hash_to_str(MENU_ENUM_LABEL_VALUE_DISK_IMAGE_APPEND),
+            msg_hash_to_str(MENU_ENUM_LABEL_DISK_IMAGE_APPEND),
+            MENU_ENUM_LABEL_DISK_IMAGE_APPEND,
+            MENU_SETTINGS_CORE_DISK_OPTIONS_DISK_IMAGE_APPEND, 0, 0, NULL))
+      count++;
+
+   return count;
 }
 
 static int menu_displaylist_parse_audio_device_list(
