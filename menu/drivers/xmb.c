deced796c1763afa64999195330af3566508174e
JoseFC3000 <70484443+JoseFC3000@users.noreply.github.com>
2025-06-19 03:39:22 +0100

    Update xmb.c

--- menu/drivers/xmb.c
+++ menu/drivers/xmb.c
@@ -238,7 +238,7 @@ enum
    XMB_TEXTURE_MENU_APPLY_COG,
    XMB_TEXTURE_DISC,
    XMB_TEXTURE_LAST,
-   XMB_TEXTURE_NO_ENTRIES_TO_DISPLAY
+   XMB_TEXTURE_NO_ENTRIES
 };
 
 enum
@@ -2943,7 +2943,7 @@ static uintptr_t xmb_icon_get_id(xmb_handle_t *xmb,
       case MENU_ENUM_LABEL_DISK_TRAY_INSERT:
          return xmb->textures.list[XMB_TEXTURE_DISK_INSERT];
       case MENU_ENUM_LABEL_NO_ENTRIES_TO_DISPLAY:
-         return xmb->textures.list[XMB_TEXTURE_NO_ENTRIES_TO_DISPLAY];
+         return xmb->textures.list[XMB_TEXTURE_NO_ENTRIES];
       case MENU_ENUM_LABEL_DISK_IMAGE_APPEND:
       case MENU_ENUM_LABEL_DISK_INDEX:
          return xmb->textures.list[XMB_TEXTURE_DISK_CURRENT];
@@ -7033,7 +7033,7 @@ static const char *xmb_texture_path(unsigned id)
          return "insert_disk.png";
       case XMB_TEXTURE_DISK_CURRENT:
          return "current_disk.png";
-      case XMB_TEXTURE_NO_ENTRIES_TO_DISPLAY:
+      case XMB_TEXTURE_NO_ENTRIES:
          return "no_entries.png";
       case XMB_TEXTURE_SHADER_OPTIONS:
          return "core-shader-options.png";
